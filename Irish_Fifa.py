83103f74a0fc4c292cbab1ba681665b9c758fb3b
BrianOSullivan <brian.osullivan@hse.ie>
2022-01-05 08:51:06 +0000

    Irish Nationality and wages in the premier league and outside

--- Irish_Fifa.py
+++ Irish_Fifa.py
@@ -8,9 +8,11 @@ print(FifaAll_Players_DF.head()) #top records
 print(FifaAll_Players_DF.dtypes) #dataframe data tyoes
 
 # Irish players in the premier league - wages
+print("******************* Irish players in the premier league - wages - Begin ********************")
 Irish_Players_PL_DF = FifaAll_Players_DF[(FifaAll_Players_DF.nationality == 'Republic of Ireland') &
                                       (FifaAll_Players_DF.league_name == 'English Premier League')]
 print(Irish_Players_PL_DF)
+print("******************* Irish players in the premier league - wages - End   ********************")
 Irish_PL_wages = Irish_Players_PL_DF.groupby('Season')['wage_eur'].agg(['mean', 'count'])
 Irish_PL_wages = Irish_PL_wages.sort_values(by = 'Season', ascending = True)
 
@@ -22,6 +24,7 @@ fig.show()
 # Irish players outside the premier league - wages
 Irish_Players_Excl_PL_DF = FifaAll_Players_DF[(FifaAll_Players_DF.nationality == 'Republic of Ireland') &
                                       (FifaAll_Players_DF.league_name != 'English Premier League')]
+
 print(Irish_Players_Excl_PL_DF)
 Irish_Excl_PL_wages = Irish_Players_Excl_PL_DF.groupby('Season')['wage_eur'].agg(['mean', 'count'])
 Irish_Excl_PL_wages = Irish_Excl_PL_wages.sort_values(by = 'Season', ascending = True)
@@ -30,3 +33,18 @@ fig1 = px.bar(Irish_Excl_PL_wages[0:10], x= Irish_Excl_PL_wages.index[0:10], y='
 fig1.update_layout(title_text='Mean wages - Irish Players outside the Premier League')
 fig1.update_xaxes(title_text="<b> Season </b>")
 fig1.show()
+
+
+Coleman_DF = FifaAll_Players_DF[(FifaAll_Players_DF.short_name == 'S. Coleman')]
+print(Coleman_DF)
+
+Coleman_Season_Grouped = Coleman_DF.sort_values(['wage_eur'],ascending=False).groupby('Season').head(3)
+print(Coleman_Season_Grouped)
+
+
+Seamus_wages = Coleman_Season_Grouped.groupby('Season')['wage_eur'].agg(['mean', 'count'])
+Seamus_wages = Seamus_wages.sort_values(by = 'Season', ascending = True)
+fig2 = px.bar(Seamus_wages[0:10], x= Seamus_wages.index[0:10], y='mean') # will show up to 10 seasons
+fig2.update_layout(title_text='Mean wages - Seamus Coleman')
+fig2.update_xaxes(title_text="<b> Season </b>")
+fig2.show()

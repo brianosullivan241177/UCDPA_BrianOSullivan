c4f259019c221bee727d84b5d1c1970b88519a26
BrianOSullivan <brian.osullivan@hse.ie>
2022-01-09 11:49:49 +0000

    Prepare file for Golden Boy awards

---
+++ Fifa_prep_GB.py
@@ -0,0 +1,14 @@
+# *********************** Brian O'Sullivan ***********************
+# ******************* Fifa Prep - Golden Boy *********************
+# *** Add the year the player won the Golden Boy award to name ***
+# * https://github.com/brianosullivan241177/UCDPA_BrianOSullivan *
+
+text = open("/users/brian/documents/FIFA_Project/Modified/FIFAAllplayers.csv", "r")
+text = ''.join([i for i in text]) \
+    .replace("M. Balotelli","GB Winner 2010 M. Balotelli").\
+    replace("M. Götze", "GB Winner 2011 M. Götze").replace("Isco", "GB Winner 2012 Isco").\
+    replace("P. Pogba", "GB Winner 2013 P. Pogba").replace("R. Sterling", "GB Winner 2014 R. Sterling")
+
+x = open("/users/brian/documents/FIFA_Project/Modified/FIFAAllplayers_GB.csv","w")
+x.writelines(text)
+x.close()
